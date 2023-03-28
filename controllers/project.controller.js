78094c6b217cc32f2f32456c30b4d0be123d91ba
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-03-28 15:14:09 -0300

    Controller e rota para apagar um projeto #8

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -35,3 +35,19 @@ export const getAllProjects = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const project = await Project.findByIdAndDelete(id)
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" })
+    }
+
+    res.status(200).json({ message: "Project deleted successfuly" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -2,12 +2,14 @@ import express from "express"
 
 import {
   createProject,
-  getAllProjects
+  getAllProjects,
+  deleteProject
 } from "../controllers/project.controller.js"
 
 const projectRouter = express.Router()
 
 projectRouter.route("/").post(createProject)
 projectRouter.route("/").get(getAllProjects)
+projectRouter.route("/:id").delete(deleteProject)
 
 export { projectRouter }
