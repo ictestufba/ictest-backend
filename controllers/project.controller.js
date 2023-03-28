5be5d8e65432a940495069ee9aebbc3b19d4838b
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-03-28 15:38:13 -0300

    Controller e rota para retornar os dados de um projeto #10

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -51,3 +51,19 @@ export const deleteProject = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const getProjectDetails = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const project = await Project.findById(id)
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" })
+    }
+
+    res.status(200).json(project)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -3,7 +3,8 @@ import express from "express"
 import {
   createProject,
   getAllProjects,
-  deleteProject
+  deleteProject,
+  getProjectDetails
 } from "../controllers/project.controller.js"
 
 const projectRouter = express.Router()
@@ -11,5 +12,6 @@ const projectRouter = express.Router()
 projectRouter.route("/").post(createProject)
 projectRouter.route("/").get(getAllProjects)
 projectRouter.route("/:id").delete(deleteProject)
+projectRouter.route("/:id").get(getProjectDetails)
 
 export { projectRouter }
