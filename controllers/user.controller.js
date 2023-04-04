e8998b3c199dd42e64643bc79bc6295c546a5124
ElieneMontenegro <lila.amaral@hotmail.com>
2023-04-04 02:06:41 -0300

    criar rota e controller de get all users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -39,3 +39,13 @@ export const getUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { createUser, getUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+} from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.route("/").post(createUser);
 userRouter.route("/:id").get(getUser);
+userRouter.route("/").get(getAllUsers);
 
 export { userRouter };
