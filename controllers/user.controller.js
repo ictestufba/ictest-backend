ff90a6fbec6041c06ec2784093d86272354c2236
ElieneMontenegro <lila.amaral@hotmail.com>
2023-04-04 02:47:17 -0300

    criação de rota e controller de deleção de user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -93,3 +93,19 @@ export const changePassword = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.deleteOne({ _id: id });
+
+    if (user.deletedCount == 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await res.status(200).json({ message: "User deleted successfuly" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
