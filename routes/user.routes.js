87b720638dfe7ac1ae103eede366e9744bbba58b
ElieneMontenegro <lila.amaral@hotmail.com>
2023-04-04 02:35:37 -0300

    criar rota para atualizar user e rota para mudar senha

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import {
+  changePassword,
   createUser,
   getAllUsers,
   getUser,
+  updateUser,
 } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
@@ -11,5 +13,7 @@ const userRouter = express.Router();
 userRouter.route("/").post(createUser);
 userRouter.route("/:id").get(getUser);
 userRouter.route("/").get(getAllUsers);
+userRouter.route("/:id").put(updateUser);
+userRouter.route("/:id").patch(changePassword);
 
 export { userRouter };
