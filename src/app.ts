2c5674e520d21fd735f55fd475f5dc95a52377ec
Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-16 15:14:44 -0300

    inicialização do prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 export const app = express()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rodrigo Cândido',
+    email: 'rodrigocan@gmail.com',
+  },
+})
