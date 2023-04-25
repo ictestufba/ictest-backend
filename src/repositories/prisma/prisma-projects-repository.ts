b23fe6da0fca0febcf200dfaea2060823074e8e5
Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-25 18:08:20 -0300

    prisma: repositório de projects

---
+++ src/repositories/prisma/prisma-projects-repository.ts
@@ -0,0 +1,77 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+
+import { ProjectsRepository } from '../projects-repository'
+
+export class PrismaProjectsRepository implements ProjectsRepository {
+  async findById(id: string) {
+    const project = await prisma.project.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return project
+  }
+
+  async create(data: Prisma.ProjectCreateInput) {
+    const project = await prisma.project.create({
+      data,
+    })
+
+    return project
+  }
+
+  async list() {
+    const projects = await prisma.project.findMany()
+
+    return projects
+  }
+
+  async findByIdAndDelete(projectId: string) {
+    throw new Error('Method not implemented.')
+  }
+
+  async findByIdAndUpdate(projectId: string, data: Prisma.ProjectUpdateInput) {
+    const project = await prisma.project.update({
+      where: {
+        id: projectId,
+      },
+      data,
+    })
+
+    return project
+  }
+
+  async addMember(projectId: string, userEmail: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    const userId = user?.id
+
+    await prisma.project.update({
+      where: {
+        id: projectId,
+      },
+      data: {
+        members: {
+          connect: {
+            user_id_project_id: {
+              project_id: projectId,
+              user_id: userId,
+            },
+          },
+        },
+      },
+    })
+
+    return userEmail
+  }
+}
