d52a58ac965e7be97af035079331d74654936031
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-27 12:53:46 -0300

    Caso de uso de informações do usuário

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
