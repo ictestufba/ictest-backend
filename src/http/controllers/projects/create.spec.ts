153fa3c01f7559475b65a2b8b5712e1a02e16379
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-28 10:32:20 -0300

    Teste E2E de criação de projeto

--- src/http/controllers/projects/create.spec.ts
+++ src/http/controllers/projects/create.spec.ts
@@ -3,7 +3,7 @@ import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
-describe('Profile (e2e)', () => {
+describe('Create Project (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,19 +12,20 @@ describe('Profile (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to get user profile', async () => {
+  it('should be able to create a project', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-      .get('/me')
+    const response = await request(app.server)
+      .post('/projects')
       .set('Authorization', `Bearer ${token}`)
-      .send()
+      .send({
+        name: 'Project 1',
+        code: 'PROJ1',
+        description: 'Some description',
+        visibility: 'private',
+        member_access: 'add_all',
+      })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'johndoe@example.com',
-      }),
-    )
+    expect(response.statusCode).toEqual(201)
   })
 })
