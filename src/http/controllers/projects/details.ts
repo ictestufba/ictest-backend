66b8c730117d41f2662decc8af776867172f48ce
Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-28 00:36:25 -0300

    Outros controllers e rotas de projetos

---
+++ src/http/controllers/projects/details.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeGetProjectDetailsUseCase } from '@/use-cases/factories/make-get-project-details-use-case'
+
+export async function details(request: FastifyRequest, reply: FastifyReply) {
+  const getProjectDetailsParamsSchema = z.object({
+    projectId: z.string().uuid(),
+  })
+
+  const { projectId } = getProjectDetailsParamsSchema.parse(request.params)
+
+  const getProjectDetailsUseCase = makeGetProjectDetailsUseCase()
+
+  await getProjectDetailsUseCase.execute({
+    projectId,
+  })
+
+  return reply.status(201).send()
+}
--- src/http/controllers/projects/routes.ts
+++ src/http/controllers/projects/routes.ts
@@ -1,7 +1,17 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { list } from './list'
+import { details } from './details'
+import { create } from './create'
+import { update } from './update'
 
 export async function projectsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/projects/list', list)
+  app.get('/projects/:projectId', details)
+
+  app.post('/projects', create)
+  app.patch('/projects/:projectId/update', update)
 }
