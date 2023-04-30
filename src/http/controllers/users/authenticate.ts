3cd771cdf6d707ea154aabce168e793b9d4eed0e
Rodrigo Cândido <rodrigocan@gmail.com>
2023-04-30 15:56:19 -0300

    Início da implementação do refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -31,9 +31,27 @@ export async function authenticate(
       },
     )
 
-    return reply.status(200).send({
-      token,
-    })
+    const refreshtoken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshtoken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
