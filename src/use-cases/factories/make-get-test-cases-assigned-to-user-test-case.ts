07bdbcc09ba2392a280a5f8eb22f004c77ed3d55
Rodrigo Cândido <rodrigocan@gmail.com>
2023-06-18 15:19:18 -0300

    fix: import do use case na factory make-get-test-cases-assigned-to-user-test-case

--- src/use-cases/factories/make-get-test-cases-assigned-to-user-test-case.ts
+++ src/use-cases/factories/make-get-test-cases-assigned-to-user-test-case.ts
@@ -1,14 +1,14 @@
 import { PrismaTestCasesRepository } from '@/repositories/prisma/prisma-test-cases-repository'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { GetTestCasesAssignedToUserUseCase } from '../get-test-cases-assigned-to-user'
+import { GetTestCasesByUserUseCase } from '../get-test-cases-by-user'
 
 export function makeGetTestCasesAssignedToUserUseCase() {
   const usersRepository = new PrismaUsersRepository()
   const testCasesRepository = new PrismaTestCasesRepository()
 
-  const useCase = new GetTestCasesAssignedToUserUseCase(
-    usersRepository,
+  const useCase = new GetTestCasesByUserUseCase(
     testCasesRepository,
+    usersRepository,
   )
 
   return useCase
