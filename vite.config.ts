ccf74964b7c16bdaba579e10e08bf0e83521a4fa
Erick Kokubum <erick.kokubum@wildlifestudios.com>
2024-04-02 21:40:31 -0300

    chore: add new jobs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
